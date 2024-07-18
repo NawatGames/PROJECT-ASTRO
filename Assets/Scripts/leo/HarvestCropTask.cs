b077d5081c8856ff718587ad67589a37189611ef
Leonardo Tagliati <leotagliati56@gmail.com>
2024-07-18 15:24:47 -0300

    .

---
+++ Assets/Scripts/leo/HarvestCropTask.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
+
+public class HarvestCropTask : TaskScript
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private float progressValue;
+    [SerializeField] private float decayValue;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    protected override void RunTask()
+    {
+        base.RunTask();
+        slider.value = 0;
+    }
+    protected override void OnUpPerformed(InputAction.CallbackContext value)
+    {
+        slider.value += progressValue;
+        if(slider.value == 1)
+        {
+            TaskSuccessful();
+        } 
+    }
+
+    protected override void TaskSuccessful()
+    {
+        base.TaskSuccessful();
+        Debug.Log("Colheita bem sucedida");
+    }
+    public override void EndTask()
+    {
+        base.EndTask();
+        StopAllCoroutines();
+    }
+
+}
