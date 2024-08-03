361983eed8f555cf2c783abe7c53cd7efc24699d
Murillo-Strina <143843776+Murillo-Strina@users.noreply.github.com>
2024-08-03 01:39:21 -0300

    Update DistributeO2Task.cs

--- Assets/Scripts/Tasks/O2/DistributeO2Task.cs
+++ Assets/Scripts/Tasks/O2/DistributeO2Task.cs
@@ -1,29 +1,97 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
 public class DistributeO2Task : TaskScript
 {
-    [SerializeField] private ArrowController _arrowController;
+    [SerializeField] private RectTransform circle;
+    [SerializeField] private RectTransform arrow;
+    [SerializeField] private RectTransform specialZone;
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float alignmentThreshold = 10f; 
 
-    private bool _taskRunning = false;
+    private bool isRotating = false;
 
-    void Update()
+    protected override void Awake()
     {
-        if (Keyboard.current.eKey.wasPressedThisFrame && !_taskRunning)
+        base.Awake();
+        PositionSpecialZone();
+    }
+
+    protected override void RunTask()
+    {
+        base.RunTask();
+        StartRotation();
+    }
+
+    private void StartRotation()
+    {
+        isRotating = true;
+    }
+
+    private void StopRotation()
+    {
+        isRotating = false;
+    }
+
+    private void RotateArrowAroundCircle()
+    {
+        arrow.RotateAround(circle.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+
+    private void CheckAlignment()
+    {
+        float angleDifference = Mathf.Abs(Vector3.SignedAngle(arrow.up, specialZone.position - circle.position, Vector3.forward));
+        if (angleDifference <= alignmentThreshold)
         {
-            StartMiniGame();
+            TaskSuccessful();
+        }
+        else
+        {
+            TaskMistakeStay();
         }
     }
 
-    private void StartMiniGame()
+    private void PositionSpecialZone()
     {
-        _taskRunning = true;
-        _arrowController.StartRotation();
+        float radius = circle.rect.width / 2;
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        Vector2 tangentialPosition = randomDirection * radius;
+
+        specialZone.localPosition = tangentialPosition;
+
+        float angle = Mathf.Atan2(randomDirection.y, randomDirection.x) * Mathf.Rad2Deg;
+        specialZone.rotation = Quaternion.Euler(0, 0, angle + 90);  
     }
 
-    private void EndMiniGame()
+    protected override void OnUpPerformed(InputAction.CallbackContext value)
     {
-        _taskRunning = false;
-        _arrowController.StopRotation();
+        if (isRotating)
+        {
+            CheckAlignment();
+        }
+    }
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            RotateArrowAroundCircle();
+        }
+    }
+
+    protected override void TaskSuccessful()
+    {
+        base.TaskSuccessful();
+        Debug.Log("Sucesso na Task");
+        PositionSpecialZone();
+        rotationSpeed +=20f;
+    }
+
+    protected override void TaskMistakeStay()
+    {
+        base.TaskMistakeStay();
+        Debug.Log("Errou");
+
     }
 }
