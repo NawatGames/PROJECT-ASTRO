06e0373735adc0d0f5c734179cac67e974f51177
Leonardo Tagliati <leotagliati56@gmail.com>
2024-08-26 15:05:19 -0300

    Arrumamos o revert do merge da descontaminacao

---
+++ Assets/Scripts/State Machine/Player/DoingDecontaminationState.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoingDecontaminationState : IPlayerState
+{
+    public void Enter(PlayerController player) 
+    {
+        player.isDoingDecontamination = true;
+        player.startedDecontaminationEvent.Raise();
+    }
+    public IPlayerState Do(PlayerController player)
+    {
+        if(player.InteractAction.WasPressedThisFrame() || !player.isDoingDecontamination)
+        {
+            player.stoppedDecontaminationEvent.Raise();
+            return player.FreeMovingState;
+        }
+        return player.DoingDecontaminationState;
+    }
+    public void Exit(PlayerController player)
+    {
+
+    }
+
+
+
+}
--- Assets/Scripts/State Machine/Player/FreeMovingState.cs
+++ Assets/Scripts/State Machine/Player/FreeMovingState.cs
@@ -15,6 +15,10 @@ public class FreeMovingState : IPlayerState
                 player.NearTaskController.wasStarted = true;
                 return player.DoingTasksState;
             }
+            if (player.IsOnLobbyArea)
+            {
+                return player.DoingDecontaminationState;
+            }
             if (player.IsOnButtonArea)
             {
                 player.NearDoorButtonController.ToggleDoor();
@@ -25,7 +29,7 @@ public class FreeMovingState : IPlayerState
             return player.GameOverState;
         }
         return player.FreeMovingState;
-        
+
     }
 
     public void FixedDo(PlayerController player)
@@ -38,11 +42,11 @@ public class FreeMovingState : IPlayerState
         _currentVelocity = Vector2.zero;
         player.Rigidbody2D.velocity = Vector2.zero;
     }
-    
+
     private void Move(PlayerController player)
     {
         _velocity = player.MoveVector * player.MoveSpeed;
-        
+
         _currentVelocity = Vector2.Lerp(_currentVelocity, _velocity, player.Acceleration * Time.fixedDeltaTime);
 
         player.Rigidbody2D.velocity = player.Rigidbody2D.velocity * player.DriftFactor + _currentVelocity * (1 - player.DriftFactor);
