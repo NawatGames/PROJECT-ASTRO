170eb77bf79ffdb6fdeebadaa89edaf11e21aef2
LucasCalcada <lucasmgmcalcada@gmail.com>
2024-09-05 20:44:18 -0300

    add comments

--- Assets/Scripts/Player/StateMachine/StateScripts/ToggleMovementOnState.cs
+++ Assets/Scripts/Player/StateMachine/StateScripts/ToggleMovementOnState.cs
@@ -8,11 +8,14 @@ namespace Player.StateMachine.StateScripts
     public class ToggleMovementOnState : BaseStateScript
     {
         [SerializeField] private PlayerMovement playerMovement;
+        
+        // Enable player movement on state enter
         protected override void EnterState()
         {
             playerMovement.enabled = true;
         }
 
+        // Disable player movement on state leave
         protected override void LeaveState()
         {
             playerMovement.enabled = false;
