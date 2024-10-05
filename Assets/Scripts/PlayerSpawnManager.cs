aa3d651550d2e9c973c519195f88b0806c5e713a
LucasCalcada <lucasmgmcalcada@gmail.com>
2024-10-05 03:04:03 -0300

    add PlayerSpawn gizmos

--- Assets/Scripts/PlayerSpawnManager.cs
+++ Assets/Scripts/PlayerSpawnManager.cs
@@ -1,21 +1,28 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerSpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
-    public Transform astroSpawn;
-    public Transform alienSpawn;
+    public Vector2 astroSpawn;
+    public Vector2 alienSpawn;
     private const string AstroControlScheme = "LeftKeyBoard";
     private const string AlienControlScheme = "RightKeyBoard";
     
-    void Awake()
+    private void Awake()
     {
         Physics2D.IgnoreLayerCollision(3, 3); // Layer Mask 3 = "Player"
         GameObject astro = PlayerInput.Instantiate(playerPrefab, 0, AstroControlScheme, default, Keyboard.current).gameObject;
-        astro.transform.position = astroSpawn.position;
+        astro.transform.position = astroSpawn;
         GameObject alien = PlayerInput.Instantiate(playerPrefab, 1, AlienControlScheme, default, Keyboard.current).gameObject;
         //alien.GetComponent<PlayerController>().isAstro = false;
-        alien.transform.position = alienSpawn.position;
+        alien.transform.position = alienSpawn;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(astroSpawn, "Astro Spawn");
+        Gizmos.DrawIcon(alienSpawn, "Alien Spawn");
     }
 }
