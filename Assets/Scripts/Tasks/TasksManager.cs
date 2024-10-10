d9c72504d47d42e64c7fb9c443b15d69808160e0
Sakaef03 <fosakae@icloud.com>
2024-10-10 20:39:12 -0300

    adicionada a lógica de pause no ShortTaskTimer()

--- Assets/Scripts/Tasks/TasksManager.cs
+++ Assets/Scripts/Tasks/TasksManager.cs
@@ -121,6 +121,7 @@ public class TasksManager : MonoBehaviour
         taskTimerTMP.text = $"{task.taskName}: {min,2}:{sec:00}";
         while (sec > 0 || min > 0)
         {
+            yield return new WaitUntil(() => task.taskScript.IsTaskInProgress() == false);
             yield return new WaitForSecondsRealtime(1);
             sec--;
             if (sec < 0)
