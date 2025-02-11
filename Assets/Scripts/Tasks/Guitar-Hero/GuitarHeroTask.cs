c1c63880ff958b6ba614460eaad05d6c652f1bf0
Lucas Calçada <lucasmgmcalcada@gmail.com>
2025-02-10 21:52:52 -0300

    remove reduntant comparison

--- Assets/Scripts/Tasks/Guitar-Hero/GuitarHeroTask.cs
+++ Assets/Scripts/Tasks/Guitar-Hero/GuitarHeroTask.cs
@@ -115,7 +115,7 @@ public class GuitarHeroTask : TaskScript
                 if (isAstro == isAstroSpecialist) pointsMade += 2;
                 else pointsMade++;
 
-                if (pointsMade == maxBlockPoints || pointsMade >= maxBlockPoints) TaskSuccessful();
+                if (pointsMade >= maxBlockPoints) TaskSuccessful();
             }
             else
             {
